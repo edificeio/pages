8f2f23ac72bdea9f1659661879d3ff5417fbcee6
dboissin <damien.boissin@web-education.net>
2015-03-13 11:33:30 +0100

    fix export resources method signature

--- src/main/java/fr/wseduc/pages/service/impl/PagesRepositoryEvents.java
+++ src/main/java/fr/wseduc/pages/service/impl/PagesRepositoryEvents.java
@@ -1,6 +1,7 @@
 package fr.wseduc.pages.service.impl;
 
 import org.entcore.common.service.impl.MongoDbRepositoryEvents;
+import org.vertx.java.core.Handler;
 import org.vertx.java.core.json.JsonArray;
 
 public class PagesRepositoryEvents extends MongoDbRepositoryEvents {
@@ -10,7 +11,7 @@ public class PagesRepositoryEvents extends MongoDbRepositoryEvents {
 	}
 
 	@Override
-	public void exportResources(String exportId, String userId, JsonArray groups, String exportPath, String locale) {
+	public void exportResources(String exportId, String userId, JsonArray groups, String exportPath, String locale, String host, final Handler<Boolean> handler) {
 
 	}
 
