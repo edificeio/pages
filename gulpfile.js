d6aeeeac46d191e3db1dc019babd855355d31326
dboissin <damien.boissin@web-education.net>
2015-12-02 15:54:31 +0100

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,21 @@
 var jshint = require('gulp-jshint');
 var gulp   = require('gulp');
+var minifyHTML = require('gulp-minify-html');
 
 gulp.task('lint', function() {
     return gulp.src('./src/main/resources/public/js/*.js')
                 .pipe(jshint())
                 .pipe(jshint.reporter('./build/resources/report-jshint'));
                                   })
+
+gulp.task('minify-html', function() {
+var opts = {
+conditionals: true,
+spare:true
+};
+
+return gulp.src('./src/main/resources/public/template/*.html')
+.pipe(minifyHTML(opts))
+.pipe(gulp.dest('./build/resources/'));
+});
 
