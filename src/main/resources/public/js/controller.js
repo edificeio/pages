ac6f648499d7bc333fe1281642ecb01c8dde8fd2
Camille Wintz <camille.wintz@web-education.net>
2016-02-02 15:09:31 +0100

    replace special characters

--- src/main/resources/public/js/controller.js
+++ src/main/resources/public/js/controller.js
@@ -294,7 +294,16 @@ function PagesController($scope, template, route, model, date, $location, $timeo
 			return;
 		}
 		$scope.page.useTemplate($scope.website, templateName);
-		$scope.page.titleLink = encodeURIComponent(lang.removeAccents($scope.page.title.replace(/\ /g, '-').replace(/:/g, '-').replace(/\?/g, '')).toLowerCase());
+		$scope.page.titleLink = encodeURIComponent(lang.removeAccents(
+            $scope.page.title
+                .replace(/\ /g, '-')
+                .replace(/:/g, '-')
+                .replace(/\?/g, '')
+                .replace(/#/g, 'hash')
+                .replace(/%/g, 'percent')
+                .replace(/$/g, 'dollar')
+                .replace(/£/g, 'pound')
+        ).toLowerCase());
 		$scope.website.pages.push($scope.page);
 		if($scope.website.pages.length() === 1){
 			$scope.website.landingPage = $scope.page.titleLink;
